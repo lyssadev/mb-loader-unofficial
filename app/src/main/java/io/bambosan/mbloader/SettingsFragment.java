fed38b8e851da0f21b103801963f3959da824494
toolsplus1000 <toolsplus1000@gmail.com>
2025-04-05 20:32:42 +0000

    feat: add beta label TextView to fragment_settings layout; import TextView in SettingsFragment for future use

--- app/src/main/java/io/bambosan/mbloader/SettingsFragment.java
+++ app/src/main/java/io/bambosan/mbloader/SettingsFragment.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import android.content.Intent;
 import androidx.fragment.app.Fragment;
 import com.google.android.material.switchmaterial.SwitchMaterial;
+import android.widget.TextView;
 
 public class SettingsFragment extends Fragment {
     private SharedPreferences prefs;
